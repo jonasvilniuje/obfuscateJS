764a0d4597ba9582b7ad3627bb67fa9b4cbfb7e1
jonasvilniuje <jonasvilniuje@gmail.com>
2021-02-09 14:51:49 +0200

    initial commit

---
+++ example.js
@@ -0,0 +1,12 @@
+/*exports.sum = function(a, b) {
+    return a + b;
+};
+*/
+
+function sum(a, b) {
+    return a + b;
+}
+
+module.exports = {
+    sum
+};
---
+++ index.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+const obf = require('./obfuscator');
+
+obf.obfuscate('./cars.json');
