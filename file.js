175cd9c61bd2fcd7908bebf80d61f1720f6c3293
jonasvilniuje <jonasvilniuje@gmail.com>
2021-02-22 14:25:47 +0200

    add comments

--- file.js
+++ file.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
-const util = require('util');
 
 /**
- * @param fileName name of file to read
- * @param callback callback function
- * @returns json file contents
+ * reads data from an input file, parses to JSON
+ * @param {string} fileName name of file to read
+ * @param {fuction} callback callback function
+ * @returns {null} none
  */
 function readFileContentJSONCallback(fileName, callback) {
     fs.readFile(fileName, 'utf-8', (err, data) => {
@@ -17,7 +17,11 @@ function readFileContentJSONCallback(fileName, callback) {
 }
 
 /**
- * writes data to file
+ * writes JSON to an output file
+ * @param {string} fileName name of file to read
+ * @param {object} data JSON object to write to a file
+ * @param {fuction} callback callback function
+ * @returns {null} none
  */
 function writeToFileCallback(fileName, data, callback) {
     fs.writeFile(fileName, JSON.stringify(data, null, 2), (err) => {
